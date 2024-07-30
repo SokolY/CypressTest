9d4db04ba3c64f2ae7583098a35a14d19cc0a5d0
Yurii Sokolovskyi <yura.sokol@gmail.com>
2024-07-30 13:34:49 +0300

    Push check2

--- cypress/e2e/fileUpload.spec.js
+++ cypress/e2e/fileUpload.spec.js
@@ -8,6 +8,6 @@ describe('Files Upload', () => {
   it('Try to Upload Correct file', () => {
     cy.visit('https://practice-automation.com/file-upload/');
     cy.get('#file-upload').selectFile('cypress/fixtures/test.jpg');
-    cy.get('#file-upload').invoke('prop', 'value').should('contain', 'test2.jpg');
+    cy.get('#file-upload').invoke('prop', 'value').should('contain', 'test3.jpg');
   })
 })
